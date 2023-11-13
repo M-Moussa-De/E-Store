d8582900f79294c94a3f27623b815556bdb40185
M-Moussa-De <mamo1611@hotmail.com>
2023-11-13 20:36:26 +0100

    Feat: Config CORS

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -22,6 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
@@ -37,7 +43,7 @@ try
 }
 catch (Exception ex)
 {
-  logger.LogError(ex, "A propblem occurred during migration");    
+    logger.LogError(ex, "A propblem occurred during migration");
 }
 
 app.Run();
