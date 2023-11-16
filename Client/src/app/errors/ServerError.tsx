8b3677cfb4566d7f0280c37ef6336b7b459b454a
M-Moussa-De <mamo1611@hotmail.com>
2023-11-16 16:35:36 +0100

    Feat: Create server error page

---
+++ Client/src/app/errors/ServerError.tsx
@@ -0,0 +1,23 @@
+import { Container, Divider, Paper, Typography } from "@mui/material";
+import { useLocation } from "react-router";
+
+export default function ServerError() {
+  const {state} = useLocation();
+
+
+  return (
+    <Container component={Paper}>
+      {state?.error ? (
+        <>
+        <Typography gutterBottom variant="h3" color='secondary'>
+          {state.error.title}
+        </Typography>
+        <Divider />
+        <Typography variant="body1"> {state.error.detail || 'Internal server errro'} </Typography>
+        </>
+      ): (
+      <Typography gutterBottom variant='h5'>Server error</Typography>
+      )}
+    </Container>
+  )
+}
