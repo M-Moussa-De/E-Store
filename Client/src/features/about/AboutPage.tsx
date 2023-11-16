f7841511ab662fc74bc16fca74f9767334356be6
M-Moussa-De <mamo1611@hotmail.com>
2023-11-16 15:20:17 +0100

    Feat: Create the UI for errors testing

--- Client/src/features/about/AboutPage.tsx
+++ Client/src/features/about/AboutPage.tsx
@@ -1,5 +1,18 @@
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import agent from "../../app/api/agent";
+
 export default function AboutPage() {
+  
   return (
-    <div>AboutPage</div>
+    <Container>
+      <Typography variant='h5' gutterBottom>Errors for testing purposes</Typography>
+      <ButtonGroup fullWidth>
+        <Button variant="contained" onClick={() => agent.TestErrors.get400Error()}>Test 400 Error</Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get401Error()}>Test 401 error</Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get404Error()}>Test 404 error</Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.get500Error()}>Test 500 error</Button>
+        <Button variant="contained" onClick={() => agent.TestErrors.getValidationError()}>Test Validation error</Button>
+      </ButtonGroup>
+    </Container>
   )
 }
