5097c68f1a263eff4d07178f1c5a5f3a6401b4d0
M-Moussa-De <mamo1611@hotmail.com>
2023-11-20 11:26:40 +0100

    Ref: Use the loading component

--- Client/src/features/catalog/Catalog.tsx
+++ Client/src/features/catalog/Catalog.tsx
@@ -2,15 +2,20 @@ import { useState, useEffect } from "react";
 import { Product } from "../../app/models/Product";
 import ProductList from "./ProductList";
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 
 export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
      agent.Catalog.list()
      .then(products => setProducts(products))
-  }, []);  
+     .finally(() => setLoading(false))
+  }, []);
+
+  if(loading) return <LoadingComponent message='Loading products...' />
   
   return (
     <>
